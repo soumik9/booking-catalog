7cb4e23527bf5711231992313479fddc0f3a2986
soumik9 <soumik.ahammed.9@gmail.com>
2023-07-16 01:52:26 +0600

    filter searable books

--- server/app/controllers/book/GetBooks.ts
+++ server/app/controllers/book/GetBooks.ts
@@ -4,12 +4,52 @@ import httpStatus from 'http-status'
 import sendResponse from '../../../utils/SendResponse';
 import Book from '../../models/BookModel';
 import { IBook } from '../../../interfaces/modelTypes';
+import { bookFilterableFields, bookSearchableFields, paginationProps } from '../../../utils/constatnts';
+import { SortOrder } from 'mongoose';
+import pick from '../../../utils/pick';
+import calculatePagination from '../../../utils/helpers/calculatePagination';
 
 const GetBooks: RequestHandler = catchAsync(
     async (req: Request, res: Response) => {
+        const andConditions: any = [];
+        const filters = pick(req.query, bookFilterableFields);
+        const { searchTerm, minPrice, maxPrice, ...filtersData } = filters;
+
+        // pagination and sorting
+        const paginationOptions = pick(req.query, paginationProps);
+        const { limit, page, skip, sortBy, sortOrder } = calculatePagination(paginationOptions);
+
+        // sorting
+        const sortConditions: { [key: string]: SortOrder } = {};
+        if (sortBy && sortOrder) { sortConditions[sortBy] = sortOrder; }
+
+        // if there is searching query
+        if (searchTerm) {
+            andConditions.push({
+                $or: bookSearchableFields.map(field => ({
+                    [field]: {
+                        $regex: searchTerm,
+                        $options: 'i',
+                    },
+                })),
+            });
+        }
+
+        // if any filterable query make it on object
+        if (Object.keys(filtersData).length) {
+            andConditions.push({
+                $and: Object.entries(filtersData).map(([field, value]) => ({
+                    [field]: value,
+                })),
+            });
+        }
+
+
+        // finalizing the where condition
+        const whereConditions = andConditions.length > 0 ? { $and: andConditions } : {};
 
         // get books
-        const result = await Book.find();
+        const result = await Book.find(whereConditions).sort(sortConditions).skip(skip).limit(limit);
         const totalRecords = await Book.countDocuments();
 
         sendResponse<IBook[]>(res, {
@@ -17,6 +57,8 @@ const GetBooks: RequestHandler = catchAsync(
             success: true,
             message: 'Books retrieved successfully!',
             meta: {
+                page,
+                limit,
                 total: totalRecords,
             },
             data: result,
--- server/utils/constatnts/index.ts
+++ server/utils/constatnts/index.ts
@@ -2,3 +2,6 @@ export const CUserRole: string[] = ['user'];
 
 // pagination props
 export const paginationProps: string[] = ['page', 'limit', 'sortBy', 'sortOrder'];
+
+export const bookFilterableFields: string[] = ['searchTerm', 'title', 'author'];
+export const bookSearchableFields: string[] = ['title', 'author']
