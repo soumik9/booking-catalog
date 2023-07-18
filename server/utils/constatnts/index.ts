7cde0a8042c6c5bc385b0106c68207e4a69c0b6c
soumik9 <soumik.ahammed.9@gmail.com>
2023-07-18 17:37:34 +0600

    fix search terms field

--- server/utils/constatnts/index.ts
+++ server/utils/constatnts/index.ts
@@ -4,4 +4,4 @@ export const CUserRole: string[] = ['user'];
 export const paginationProps: string[] = ['page', 'limit', 'sortBy', 'sortOrder'];
 
 export const bookFilterableFields: string[] = ['searchTerm', 'title', 'author'];
-export const bookSearchableFields: string[] = ['title', 'author']
+export const bookSearchableFields: string[] = ['title', 'author', 'genre']
